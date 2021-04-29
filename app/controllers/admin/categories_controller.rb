1fe8ce813b2e9a3d2c836be9e1f3b3eb0fe1cecf
Karina Song <ksysong@gmail.com>
2021-04-29 18:38:11 +0000

    add prelim code to controller and create view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,14 @@
 class Admin::CategoriesController < ApplicationController
+  # Basic HTTP Authentication
+  http_basic_authenticate_with name: ENV["ADMIN_HTTP_BASIC_USER"], password: ENV["ADMIN_HTTP_BASIC_PASSWORD"]
+
+  def index
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+  end
 end
