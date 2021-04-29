35d306b05f8bfeeab2215e5c90042e5fb68af6c9
Karina Song <ksysong@gmail.com>
2021-04-29 18:46:33 +0000

    add links to admin/categories to dashboard and nav

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_HTTP_BASIC_USER"], password: ENV["ADMIN_HTTP_BASIC_PASSWORD"]
 
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
