82fc7229bcfd03236fb4d1d3cb9d00d57b87b5de
Karina Song <ksysong@gmail.com>
2021-04-30 03:35:00 +0000

    add logic for log in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    
+    if user && user.authenticate(params[:password])
+      # Set the browser's cookies
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
   
 end
