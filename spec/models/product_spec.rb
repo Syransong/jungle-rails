3a587488998118525eb7db21dc5f8bb5ac4a7c39
Karina Song <ksysong@gmail.com>
2021-05-04 02:52:39 +0000

    successfuly create and pass first test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    
+    context "given all 4 product fields: name, price, quantity and category" do
+      it "saves successfully" do
+        @category = Category.new
+        @product = Product.new(
+          name: "Mechanical Keyboard",
+          price: 450.00,
+          quantity: 7,
+          category: @category
+        )
+        expect(@product).to be_valid
+      end
+    end
   end
 end
+
+# validation tests/examples here
+
+    # validates :name, presence: true
+    # validates :price, presence: true
+    # validates :quantity, presence: true
+    # validates :category, presence: true
