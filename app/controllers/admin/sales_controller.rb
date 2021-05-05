3a302ba1bbf78cbad098b7749dd58cb41ee73a3b
Karina Song <ksysong@gmail.com>
2021-05-05 20:31:21 +0000

    add user authentication to sales controller

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,5 +1,10 @@
 class Admin::SalesController < ApplicationController
+  # Basic HTTP Authentication
+  http_basic_authenticate_with name: ENV["ADMIN_HTTP_BASIC_USER"], password: ENV["ADMIN_HTTP_BASIC_PASSWORD"]
 
+  # Bcrypt User Authentication 
+  before_filter :authorize
+  
   def index
     @sales = Sale.all
     #implicitly will render index 
